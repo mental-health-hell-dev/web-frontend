43d5261bd3238be16321e38c6ba93b28b0e13e01
Barmatov Alexey <82975661+hellpes666@users.noreply.github.com>
2025-08-02 20:48:49 +0300

    init

---
+++ app/modules/header/ui/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className="w-full flex items-center h-full relative ">
+            <div className="absolute w-full h-full isolate aspect-video rounded bg-white/20 shadow-lg ring-1 ring-black/5 backdrop-blur-xl hidden z-10 sm:flex sm:items-center sm:px-8 justify-between">
+                <div className="">hell</div>
+                <nav className="">main,</nav>
+                <div className="">hell</div>
+            </div>
+        </header>
+    );
+};
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Header } from "./modules";
+
+export default function Home() {
+    return (
+        <div className="font-fredoka-sans grid grid-rows-[50px_1fr_20px] items-center justify-items-center min-h-screen pt-12 px-4 pb-20 gap-16 sm:px-12 sm:py-6 text-[#4B2C5E]">
+            <Header />
+        </div>
+    );
+}
